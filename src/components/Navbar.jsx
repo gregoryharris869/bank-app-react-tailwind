5d0210505e07b10cb5475e5bce571c7e850bb08a
Gregory Harris <63122765+gregoryharris869@users.noreply.github.com>
2023-11-26 15:21:08 -0600

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,10 +8,10 @@ const Navbar = () => {
   const [toggle, setToggle] = useState(false);
 
   return (
-    <nav className="w-full flex py-6 justify-between items-center navbar">
+    <nav className="flex items-center justify-between w-full py-6 navbar">
       <img src={logo} alt="hoobank" className="w-[124px] h-[32px]" />
 
-      <ul className="list-none sm:flex hidden justify-end items-center flex-1">
+      <ul className="items-center justify-end flex-1 hidden list-none sm:flex">
         {navLinks.map((nav, index) => (
           <li
             key={nav.id}
@@ -25,7 +25,7 @@ const Navbar = () => {
         ))}
       </ul>
 
-      <div className="sm:hidden flex flex-1 justify-end items-center">
+      <div className="flex items-center justify-end flex-1 sm:hidden">
         <img
           src={toggle ? close : menu}
           alt="menu"
@@ -38,7 +38,7 @@ const Navbar = () => {
             !toggle ? "hidden" : "flex"
           } p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
         >
-          <ul className="list-none flex justify-end items-start flex-1 flex-col">
+          <ul className="flex flex-col items-start justify-end flex-1 list-none">
             {navLinks.map((nav, index) => (
               <li
                 key={nav.id}

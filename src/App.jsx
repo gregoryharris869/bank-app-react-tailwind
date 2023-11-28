dc45f36a1992b03767e48d88f6c8151fc1156e5a
Gregory Harris <63122765+gregoryharris869@users.noreply.github.com>
2023-11-28 16:37:57 -0600

    Hero section complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,33 @@
 import styles from "./styles";
-import Navbar from "./components/Navbar";
+import { Navbar, Hero } from "./components";
 
-const App = () => {
-  return (
-    <div className="bg-primary w-full overflow-hidden">
-      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
-        <div className={`${styles.boxWidth}`}>
-          <Navbar />
-        </div>
+const App = () => (
+  <div className="w-full overflow-hidden bg-primary">
+    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Navbar />
       </div>
     </div>
-  );
-};
+
+    <div className={`bg-primary ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Hero />
+      </div>
+    </div>
+
+    <div className={`bg-primary ${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+        {/* <Stats />
+        <Business />
+        <Billing />
+        <CardDeal />
+        <Testimonials />
+        <Clients />
+        <CTA />
+        <Footer /> */}
+      </div>
+    </div>
+  </div>
+);
 
 export default App;
